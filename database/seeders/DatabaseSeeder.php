f4b2fbe9f1a4d3806bea56eecf53f1bc36dffd32
cinthiaTG <2230282@upv.edu-mx>
2025-02-02 16:49:59 -0600

    login y seeders de usuarios

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,10 +15,29 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+        $this->call([
+            //UserSeeder::class,
+            //UserSeeder::class,
+            //UserSeeder::class,
+        ]);
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Cinthia Admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('password'),
+            'rol' => 'admin',
+        ]);
+        User::factory()->create([
+            'name' => 'Luis User',
+            'email' => 'luis@gmail.com',
+            'password' => Hash::make('password'),
+            'rol' => 'usuario',
+        ]);
+        User::factory()->create([
+            'name' => 'Juanjo transportista',
+            'email' => 'juanjo@gmail.com',
+            'password' => Hash::make('password'),
+            'rol' => 'transportista',
         ]);
     }
 }
